5898aaea1248f5ba8a8678619996bdb67a1b769a
Rekha-15 <rekhapatil.1509@gmail.com>
2021-06-20 21:13:03 -0400

    add : welcome statement : .

---
+++ src/main/java/com/UserRegistration_junit/UserRegistration.java
@@ -0,0 +1,22 @@
+/**
+ *@purpose Implementing User Registration Program By testing in junit
+ *@author rekha
+ *@version 1.0
+ *@since 20-06-2021
+ *
+ */
+
+package com.UserRegistration_junit;
+
+/**
+ * UserRegistration is a class of public type
+ * @author rekha
+ *Printing welcome statement
+ */
+public class UserRegistration {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to User Registration Progran ");
+	}
+}
